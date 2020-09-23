06d56a8d3c218089e69dde29efb3e6522e9b07a4
todd-estes <elestes@gmail.com>
2020-09-23 09:59:28 -0600

    Add ingredients method to recipe.rb

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,7 +1,19 @@
 class Recipe
   attr_reader :name, :ingredients_required
   def initialize(name)
-    @name = name 
+    @name = name
     @ingredients_required = Hash.new(0)
   end
+
+  def add_ingredient(ingredient_obj, quantity)
+    @ingredients_required[ingredient_obj] += quantity
+  end
+
+  def ingredients
+    # ingredient_hash
+    @ingredients_required.map do |ingredient_obj, quantity|
+      ingredient_obj
+    end
+  end
+
 end
