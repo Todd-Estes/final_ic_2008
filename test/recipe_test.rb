287f12e969a5d0928bb52cd4a7373a022ca3eea0
todd-estes <elestes@gmail.com>
2020-09-23 09:46:48 -0600

    Add add_ingredient and ingredient method tests to recipe_test.rb

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -12,4 +12,19 @@ class RecipeTest < Minitest::Test
      expected = {}
      assert_equal expected, recipe1.ingredients_required
    end
+
+  def it_can_add_required_ingedients
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe1.name
+   recipe1.ingredients_required
+   recipe1.add_ingredient(ingredient1, 2)
+   recipe1.add_ingredient(ingredient1, 4)
+   recipe1.add_ingredient(ingredient2, 8)
+   recipe1.ingredients_required
+     # => {#<Ingredient:0x00007fd7811553c8...> => 6, #<Ingredient:0x00007fd78110b0e8...> => 8}
+   # recipe1.ingredients
+   # # => [#<Ingredient:0x007fe8438c7a70...>, #<Ingredient:0x007fe843857f40...>]
+ end
 end
