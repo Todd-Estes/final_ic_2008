decfb008dbc711d120cd03e09275cc6a0d7977cc
todd-estes <elestes@gmail.com>
2020-09-23 09:23:27 -0600

    Add stock and restock method tests to pantry_test.rb

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -7,7 +7,7 @@ class PantryTest < Minitest::Test
   def test_it_exists_with_attributes
      pantry = Pantry.new
      expected = {}
-     assert_instance_of Pantry, pantry.new
+     assert_instance_of Pantry, pantry
      assert_equal expected, pantry.stock
 
      # assert_equal 0, pantry.stock_check(ingredient1)
@@ -17,4 +17,15 @@ class PantryTest < Minitest::Test
      # pantry.restock(ingredient2, 7)
      # assert_equal 7, pantry.stock_check(ingredient2)
    end
- end 
+   def test_it_can_check_stock_and_restock
+      ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+      ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+      pantry = Pantry.new
+      assert_equal 0, pantry.stock_check(ingredient1)
+      pantry.restock(ingredient1, 5)
+      pantry.restock(ingredient1, 10)
+      assert_equal 15, pantry.stock_check(ingredient1)
+      pantry.restock(ingredient2, 7)
+      assert_equal 7, pantry.stock_check(ingredient2)
+    end
+ end
