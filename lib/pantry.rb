782c28c1f10bbb0fc54ccb3882ef2077bccef930
todd-estes <elestes@gmail.com>
2020-09-23 10:58:40 -0600

    Add enough_ingredients_for? method to pantry.rb

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,4 @@
+require './lib/recipe'
 class Pantry
   attr_reader :stock
   def initialize
@@ -7,4 +8,21 @@ class Pantry
   def stock_check(ingredient_obj)
     @stock[ingredient_obj]
   end
+
+  def restock(ingredient_obj, quantity)
+    @stock[ingredient_obj] += quantity
+  end
+
+  def enough_ingredients_for?(recipe_obj)
+    @stock.each do |ingredient_obj, quantity|
+      recipe_obj.ingredients_required.each do |rec_ing, rec_quan|
+        if ingredient_obj == rec_ing && quantity >= rec_quan
+          return true
+        else
+          false
+        end
+      end
+    end
+  end
+
 end
